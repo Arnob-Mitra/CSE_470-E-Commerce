9341a3197b385a7643e9ca98a7e15b1e1b96ee46
Arnob Mitra Utsab <62831092+Arnob-Mitra@users.noreply.github.com>
2022-12-17 22:49:29 +0600

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,95 @@
+const data = {
+
+}
+export const sliderItems =[
+    {
+        _id: 1,
+        image: '/images/slide04.jpg'
+    },
+
+    {
+        _id: 2,
+        image: '/images/slide05.jpg'
+    },
+
+    {
+        _id: 3,
+        image: '/images/slide06.jpg'
+    }
+
+
+];
+
+export const category = [
+    {
+        _id:1,
+        title: 'Accesories',
+        image: '/images/Headphones.jpg'
+    },
+
+    {
+        _id:2,
+        title: 'Tablet',
+        image: '/images/Tablet.jpg'
+    },
+
+    {
+        _id:3,
+        title: 'Mobile Phones',
+        image: '/images/Phones.jpg'
+    },
+
+];
+
+export const products=[
+    {
+        _id: 1,
+        title:'Iphone 13 Pro Max',
+        slug: 'product1',
+        desc: 'Description for Product',
+        category: 'Mobile Phones',
+        image:'/images/p1.jpg',
+        price: '163,000 taka',
+
+    },
+    {
+        _id: 2,
+        title:'Samsung s7 Tablet',
+        slug: 'product2',
+        desc: 'Description for Product',
+        category: 'Tablet',
+        image:'/images/p2.jpg',
+        price: '58,000 taka',
+
+    },
+    {
+        _id: 3,
+        title:'Pixel 7',
+        slug: 'product3',
+        desc: 'Description for Product',
+        category: 'Mobile Phones',
+        image:'/images/p3.jpg',
+        price: '75,000 taka',
+
+    },
+    {
+        _id: 4,
+        title:'Airpod Max',
+        slug: 'product4',
+        desc: 'Description for Product',
+        category: 'Accesories',
+        image:'/images/p4.jpg',
+        price: '65,000 taka',
+
+    },
+    {
+        _id: 5,
+        title:'ESR Screen Protector',
+        slug: 'product5',
+        desc: 'Description for Product', 
+        category: 'Accesories',
+        image:'/images/p5.jpg',
+        price: '500 taka',
+
+    },
+];
